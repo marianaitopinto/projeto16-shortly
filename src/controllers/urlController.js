b0060ee32ab9dd04f828c04181f178b58757b9a4
Mariana Ito <marianaitopinto@gmail.com>
2022-06-10 11:35:18 -0300

    getting shorUrl

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -1,8 +1,8 @@
-import db from  '../db.js';
+import db from '../db.js';
 import { nanoid } from 'nanoid';
 
 export async function shortUrl(req, res) {
-    const {url} = req.body;
+    const { url } = req.body;
     const userId = res.locals.user.id;
 
     const shortUrl = nanoid(8);
@@ -14,7 +14,7 @@ export async function shortUrl(req, res) {
         VALUES ($1, $2, $3)
         `, [userId, url, shortUrl]);
 
-        res.status(201).send({shortUrl: shortUrl});
+        res.status(201).send({ shortUrl: shortUrl });
 
     } catch (error) {
         res.status(500).send(error);
@@ -22,7 +22,7 @@ export async function shortUrl(req, res) {
 }
 
 export async function getUrlById(req, res) {
-    const {id} = req.params;
+    const { id } = req.params;
 
     try {
         const { rows: urls } = await db.query('SELECT id, "shortUrl", url FROM urls WHERE id=$1', [id])
@@ -31,6 +31,30 @@ export async function getUrlById(req, res) {
         if (!url) return res.sendStatus(404);
 
         res.status(200).send(url);
+
+    } catch (error) {
+        res.status(500).send(error);
+    }
+}
+
+export async function operUrl(req, res) {
+    const { shortUrl } = req.params;
+
+    try {
+        const { rows: urls } = await db.query(`SELECT * FROM urls WHERE "shortUrl"=$1`, [shortUrl]);
+        const [url] = urls;
+        const visits = url.visits;
+        const normalUrl = url.url;
+
+        if (!url) return res.sendStatus(404);
+
+        await db.query(`
+        UPDATE urls
+        SET visits=$1
+        WHERE "shortUrl"=$2
+        `, [parseInt(visits) + 1, shortUrl]);
+
+        res.redirect(normalUrl);
 
     } catch (error) {
         res.status(500).send(error);
--- src/routes/urlsRouter.js
+++ src/routes/urlsRouter.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 
-import { shortUrl, getUrlById } from '../controllers/urlController.js';
+import { shortUrl, getUrlById, operUrl } from '../controllers/urlController.js';
 import {validateSchema} from '../middlewares/schemaMiddleware.js'
 import validateToken from '../middlewares/tokenMiddleware.js';
 import urlSchema from '../schemas/urlSchema.js';
@@ -9,5 +9,6 @@ const urlRouter = Router();
 
 urlRouter.post('/urls/shorten', validateToken, validateSchema(urlSchema), shortUrl)
 urlRouter.get('/urls/:id', getUrlById)
+urlRouter.get('/urls/open/:shortUrl', operUrl);
 
 export default urlRouter;
