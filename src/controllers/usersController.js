6e0a4c50f23a33118dffbdb379f2a012bb64aff2
Mariana Ito <marianaitopinto@gmail.com>
2022-06-10 13:39:14 -0300

    getting user urls

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,2 +1,37 @@
 import db from "../db.js";
 
+export async function getUser(req, res) {
+    const { id } = req.params;
+    const userId = res.locals.user.id;
+
+    try {
+        const { rows: urls } = await db.query('SELECT * FROM users WHERE id=$1', [id])
+        const [url] = urls;
+
+        if (!url) return res.sendStatus(404);
+        if (Number(id) !== Number(userId)) return res.sendStatus(401);
+
+        const { rows: userInfo } = await db.query(`
+        SELECT users.id, users.name, sum(urls.visits) as "VisitCount" from users
+        LEFT JOIN urls ON users.id = urls."userId"
+        WHERE users.id = $1
+        GROUP BY users.id;
+        `, [id]);
+
+        console.log(userInfo)
+
+        const {rows: userUrls } = await db.query(`
+        SELECT id, "shortUrl", url, visits AS "visitCount"
+        FROM urls
+        WHERE "userId"=$1
+        `, [id]);
+
+        const shortenedUrls = userUrls;
+        const userResponse = {...userInfo[0], shortenedUrls};
+
+        res.status(200).send(userResponse);
+
+    } catch (error) {
+        res.status(500).send(error);
+    }
+}
--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -1,7 +1,10 @@
 import { Router } from 'express';
 
+import { getUser } from '../controllers/usersController.js'
+import validateToken from '../middlewares/tokenMiddleware.js';
 
 const usersRouter = Router();
 
+usersRouter.get('/users/:id', validateToken, getUser);
 
 export default usersRouter;
